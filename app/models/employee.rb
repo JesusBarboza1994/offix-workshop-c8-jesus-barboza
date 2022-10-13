a0f00597011c1587d0f215abacd59a6c84c1cb8a
Jesus Barboza <jbarz20@gmail.com>
2022-10-13 10:04:44 -0500

    validations of employee.

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,12 @@
 class Employee < ApplicationRecord
+  # Associations
   belongs_to :department
+
+  # Validations
+  validates :name, presence: true
+  validates :nationality, presence: true
+  validates :role, presence: true
+  validates :birth_date, comparison: { less_than_or_equal_to: 18.years.ago, message: "#{birth_date} you should be older than 18" }
+
+  # Callbacks
 end
